d055b856c7d907a0685efb4018263dfa0458c98e
3152215960 <3152215960@BRRLI0512>
2017-09-26 20:00:26 -0300

    CalculaBonus

--- RefactingForTesting/RefactingForTesting/CalculaBonus.cs
+++ RefactingForTesting/RefactingForTesting/CalculaBonus.cs
@@ -19,32 +19,20 @@ namespace RefactingForTesting
             return listaBonus.FirstOrDefault(b => aTempoCasa < b.TempoCasa).CalcularBonus(aValor);
         }
 
-        //public Dictionary<BancoEnum, decimal> CalculaBonus(IEnumerable<IGrouping<BancoEnum, Funcionario>> aLista)
-        //{
-        //    var retorno = new Dictionary<BancoEnum, decimal>();
-        //    foreach (var banco in aLista)
-        //    {
-        //        var valor = 0M;
-        //        foreach (var func in banco)
-        //        {
-        //            //var bonus = 0.0M;
-        //            //var tempoDeCasa = hoje.Year - func.Admissao.Year;
-        //            //if (tempoDeCasa < 3)
-        //            //    bonus = 0;
-        //            //else
-        //            //    if (tempoDeCasa < 5)
-        //            //    bonus = 0.03M;
-        //            //else
-        //            //        if (tempoDeCasa < 10)
-        //            //    bonus = 0.05M;
-        //            //else
-        //            //    bonus = 0.1M;
-
-        //            //valor += Math.Round(func.Salario * bonus, 2);
-        //        }
-        //        retorno[banco.Key] = valor;
-        //    }
-        //    return retorno;
-        //}
+        public Dictionary<BancoEnum, decimal> CalculaBonusPorBanco(IEnumerable<IGrouping<BancoEnum, Funcionario>> aLista)
+        {
+            var retorno = new Dictionary<BancoEnum, decimal>();
+            foreach (var banco in aLista)
+            {
+                var valor = 0M;
+                foreach (var func in banco)
+                {
+                    var tempoDeCasa = DateTime.Now.Year - func.Admissao.Year;
+                    valor += this.CalcularBonus(tempoDeCasa, func.Salario);
+                }
+                retorno[banco.Key] = valor;
+            }
+            return retorno;
+        }
     }
 }
