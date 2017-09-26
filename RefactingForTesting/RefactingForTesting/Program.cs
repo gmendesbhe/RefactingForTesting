7a3173351278e86bc795db79c95cbe5ce135fdf2
jhonantan <jhonsoad@gmail.com>
2017-09-26 20:02:32 -0300

    asdas

--- RefactingForTesting/RefactingForTesting/Program.cs
+++ RefactingForTesting/RefactingForTesting/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,8 @@ namespace RefactingForTesting
     {
         static void Main(string[] args)
         {
-
             var proc = new ProcessaPagamento();
+            proc.GerarArquivoConsolidadoPagamento(new FuncionarioDados(), DateTime.Now, new StreamReader("c:\\temp\\depositos.csv"), new StreamWriter("c:\\temp\\depositos.csv"));
             //proc.GerarArquivoConsolidadoPagamento("c:\\temp\\depositos.csv");
 
 
