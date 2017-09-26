d89f80ee711400e705c086431c367fbb13f61eb9
3152215960 <3152215960@BRRLI0512>
2017-09-26 19:44:10 -0300

    Classes de bonus

---
+++ RefactingForTesting/RefactingForTesting/IBonus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactingForTesting
+{
+    public interface IBonus
+    {
+        int TempoCasa { get;}
+        decimal CalcularBonus(decimal aSalario);
+    }
+}
